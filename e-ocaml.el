e7b7af9ee54bbf9d71dd15bb558e72a6528e6bbb
ic-hacker <ichinghacker@yandex.com>
2018-06-13 02:04:43 +0800

    The option for OCaml

---
+++ e-ocaml.el
@@ -0,0 +1,21 @@
+;;; package --- Summary
+;;; Commentary:
+;;;
+;;; Code:
+(autoload 'merlin-mode "Merlin" "Merlin mode" t nil)
+
+(autoload 'tuareg-mode  "Tuareg" "Tuageg mode" t nil)
+
+(load "/home/ic_hacker/.opam/4.06.1/share/emacs/site-lisp/merlin.el")
+(load "/home/ic_hacker/.opam/4.06.1/share/emacs/site-lisp/tuareg-site-file.el")
+(load "/home/ic_hacker/.opam/4.06.1/share/emacs/site-lisp/merlin-company.el")
+
+(let ((opam-share (ignore-errors (car (process-lines "opam" "config" "var" "share")))))
+  (when (and opam-share (file-directory-p opam-share))
+    (add-to-list 'load-path (expand-file-name "emacs/site-lisp" opam-share))
+    (add-hook 'tuareg-mode-hook 'merlin-mode t)
+    (add-hook 'caml-mode-hook 'merlin-mode t)
+    (setq merlin-command 'opam)))
+
+(provide 'e-ocaml)
+;;; e-ocaml ends here
